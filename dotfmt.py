f2e0aa7ca7dc938bdc327c4cd5f9a35637e29a2f
philipobi <philipobiu@gmail.com>
2025-07-19 19:26:49 +0200

    Rerender old graphs and add some more

--- dotfmt.py
+++ dotfmt.py
@@ -12,12 +12,12 @@ def extractDefs(dotCode: str) -> tuple[str]:
     return tuple(
         item
         for defStr in (
-            re.match(
-                r"digraph \{(?P<graph>.*)\}",
+            re.search(
+                r"(?<=digraph \{).*(?=\})",
                 dotCode,
                 re.DOTALL,
             )
-            .group("graph")
+            .group(0)
             .split(";")
         )
         if (item := defStr.strip())
@@ -25,7 +25,6 @@ def extractDefs(dotCode: str) -> tuple[str]:
 
 
 def fmtDotCode(dotCode: str) -> str:
-    print("Formatting code", file=sys.stderr)
     return subprocess.run(
         ("nop",),
         input=dotCode.encode(),
@@ -33,7 +32,7 @@ def fmtDotCode(dotCode: str) -> str:
     ).stdout.decode()
 
 
-def formatNodeLabel(match: re.Match, nodeName: str) -> str:
+def formatNodeLabel(match: re.Match) -> str:
     label = match.group(0)
     [
         fileName,
@@ -43,26 +42,12 @@ def formatNodeLabel(match: re.Match, nodeName: str) -> str:
         callCount,
     ] = label.split("\\n")
 
-    funcSpec1 = None
-    for segment in nodeName.split(" "):
-        if "(" in segment:
-            [funcSpec1, *_] = segment.split("(")
-            break
-
-    scopeDefs = (funcSpec,)
-    if funcSpec1 is not None:
-        scopeDefs = funcSpec1.split("::")
-        scopeDefs = (
-            *map(lambda s: s + "::", scopeDefs[:-1]),
-            scopeDefs[-1],
-        )
-
     return reduce(
         lambda s1, s2: s1 + s2,
         map(
-            lambda s: s + "\l",
+            lambda s: s + r"\l",
             (
-                *scopeDefs,
+                funcSpec,
                 f"{totalPercentage} {selfPercentage}",
             ),
         ),
@@ -71,7 +56,6 @@ def formatNodeLabel(match: re.Match, nodeName: str) -> str:
 
 
 def strip(dotCode: str) -> tuple[str]:
-    print("Stripping code", file=sys.stderr)
     dotCode = re.sub(
         r"\b(?:nodesep|tooltip|ranksep|width|height|fontsize|labeldistance)=[^\n\]]*",
         "",
@@ -79,13 +63,9 @@ def strip(dotCode: str) -> tuple[str]:
     )
     defs = extractDefs(dotCode)
 
-    getNodeName = lambda defStr: re.match(
-        r"\s*\"(?P<name>.*?)\"", defStr, re.DOTALL
-    ).group("name")
-
     stripNode = lambda defStr: re.sub(
         r"(?<=label=\").*?(?=\")",
-        lambda match: print(defStr, file=sys.stderr) or formatNodeLabel(match, getNodeName(defStr)),
+        lambda match: formatNodeLabel(match),
         defStr,
         flags=re.DOTALL,
     )
